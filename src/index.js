59e77904c58c982afb4ca94b85515b27141e19d3
ronny <v.ronny.harianto@gmail.com>
2024-09-18 12:54:56 +0700

    add product page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
 import TicTacToe from "./tictactoe.js";
+import ProductTable from "./product.js";
 
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
     <TicTacToe />
+    <br/>
+    <ProductTable />
   </StrictMode>
 );
